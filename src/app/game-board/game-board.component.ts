aa535665827a0adf159d6de2069e5bf95200ed59
Jonathan <jgimmink@avans.nl>
2017-08-08 23:59:10 +0200

    update

--- src/app/game-board/game-board.component.ts
+++ src/app/game-board/game-board.component.ts
@@ -12,6 +12,7 @@ import { ApiService } from "../api.service";
 export class GameBoardComponent {
 
   private _tiles: Tile[]
+  private clickedTile: GameBoardTileComponent;
 
   @Input() set tiles(value: Tile[]) {
     this._tiles = value;
@@ -33,9 +34,21 @@ export class GameBoardComponent {
   }
 
   canSelect(component: GameBoardTileComponent) {
-    component.isSelected = !component.isSelected
-    var select = this.isClickable(component.tile.xPos,component.tile.yPos, component.tile.zPos)
-    console.log(select);
+    var clickable = this.isClickable(component.tile.xPos, component.tile.yPos, component.tile.zPos)
+    if (clickable) {
+      if (this.clickedTile == undefined) {
+        this.clickedTile = component;
+        this.clickedTile.isSelected = true;
+      } else {
+        var isMatch = this.MatchMaker(this.clickedTile, component);
+        this.clickedTile.isSelected = false;
+        this.clickedTile = undefined;
+
+        if (isMatch) {
+          console.log("Match!♫♦b○À♫À╣Õ♀x7");
+        }
+      }
+    }
   }
 
   private drawTile(index) {
@@ -44,47 +57,44 @@ export class GameBoardComponent {
     setTimeout(() => { this.drawTile(index + 1) });
   }
 
-  private isClickable(px,py,pz):boolean{
+  private isClickable(px, py, pz): boolean {
     var match = true;
-        for(var l = 0; l < 3; l++){
-          var tile;
-            if( tile = this.tiles.find(tile => tile.xPos == ((px -1)+l)  && tile.yPos == (py -1) &&  tile.zPos == pz) != undefined){
+    for (var l = 0; l < 3; l++) {
+      var tile;
+      if (tile = this._tiles.find(tile => tile.xPos == (px - 2) && tile.yPos == ((py - 1) + l) && tile.zPos == pz) != undefined) {
+
+        if (match) {
+          for (var r = 0; r < 3; r++) {
+            if (tile = this._tiles.find(tile => tile.xPos == (px + 2) && tile.yPos == ((py - 1) + r) && tile.zPos == pz) != undefined) {
               match = false;
-              if(tile.isMatched || tile.matchesWholeSuit){
-                match = false;
-              }
-            }   
-        }
-        if(match){
-        for(var r = 0; r < 3; r++){
-         if(tile = this.tiles.find(tile => tile.xPos == ((px -1)+r)  && tile.yPos == (py +1) &&  tile.zPos == pz) != undefined){
-              match = false;
-              if(tile.isMatched || tile.matchesWholeSuit){
-                match = false;
-              }
             }
-        }
-      }
-      if(match){
-        for(var u = 0; u < 9; u++){
-          var left = -1 + (u / 3);
-          if(tile = this.tiles.find(tile => tile.xPos == (px+left)  && tile.yPos == ((py-1) + (u % 3)) &&  tile.zPos == pz) != undefined){
-            match = false;
-            if(tile.isMatched || tile.matchesWholeSuit){
-                match = false;
-              }
           }
         }
-        } 
-     
+      }
+    }
+
+    if (match) {
+      for (var u = 0; u < 9; u++) {
+        var left = -1 + Math.floor((u / 3));
+        if (tile = this._tiles.find(tile => tile.xPos == (px + left) && tile.yPos == ((py - 1) + (u % 3)) && tile.zPos == pz + 1) != undefined) {
+          match = false;
+        }
+      }
+    }
+
     return match;
   }
-  public MatchMaker(selectedTile,compareTile): boolean{
+  public MatchMaker(selectedTile, compareTile): boolean {
     var match = false;
-      if(selectedTile.tile.name == compareTile.tile.name || selectedTile.tile.suit == compareTile.tile.suit){
-        match = true;
-      }
-    
+
+
+    if (selectedTile.tile.tile.name == compareTile.tile.tile.name && selectedTile.tile.tile.suit == compareTile.tile.tile.suit) {
+      match = true;
+    }
+
+    if (selectedTile.tile.tile.matchesWholeSuit && selectedTile.tile.tile.suit == compareTile.tile.tile.suit) {
+      match = true;
+    }
 
     return match;
   }
