b50336b6f1eb4af42004f354152ce0af3c2b5520
Jonathan <jgimmink@avans.nl>
2017-08-11 22:38:17 +0200

    update user stats

--- src/app/game-lobby/game-lobby.component.ts
+++ src/app/game-lobby/game-lobby.component.ts
@@ -60,6 +60,9 @@ export class GameLobbyComponent implements OnInit, OnDestroy {
   }
 
   matchFound(data) {
+    const player = this.game.players.find(player => player._id === data[0].match.foundBy);
+    player.numberOfMatches += 1;
+
     data.forEach(tile => {
       const found = this.game.tiles.find(_tile => _tile._id === tile._id);
       found.match = tile.match;
