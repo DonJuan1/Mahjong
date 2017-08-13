977853e7f46374f399a0b376aeb2bcc4e93c2870
photonman1997 <jmvugt1@avans.nl>
2017-08-13 20:26:52 +0200

    e2e tests

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,3 +1,4 @@
+import { browser } from 'protractor';
 import { MahjongMayhemPage } from './app.po';
 
 describe('mahjong-mayhem App', () => {
@@ -8,7 +9,30 @@ describe('mahjong-mayhem App', () => {
   });
 
   it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    page.navigateTo('/');
+    expect(page.getTitleText()).toEqual('MahjongMayhem');
   });
+  
+  it('should create a game', () => {
+    page.navigateTo('/');
+    browser.executeScript('localStorage.setItem("token","eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.ImpnLmltbWlua0BzdHVkZW50LmF2YW5zLm5sIg.DhIckJhLdwT-dfykycjGjpx6INAI78BIyuKQdrXxn3w");');
+    browser.executeScript('localStorage.setItem("email","jg.immink@student.avans.nl");');
+    page.navigateTo('/newgame');
+    page.setplayer();
+    page.Submit();
+    var result = page.getUrl();
+    browser.executeScript('localStorage.clear();');
+    expect(result).toContain('/games/all');
+    
+  });
+  
+it('should login', () => {
+    page.navigateTo('/');
+    browser.ignoreSynchronization = true;
+    page.login();
+    page.loginengine();
+    expect(browser.getCurrentUrl()).toBe('https://login.avans.nl/nidp/saml2/sso');
+  });
+  
+  
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,11 +1,27 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, protractor } from 'protractor';
 
 export class MahjongMayhemPage {
-  navigateTo() {
-    return browser.get('/');
+  navigateTo(url) {
+    return browser.get(url);
+  }
+  getUrl(){
+    return browser.getCurrentUrl();
   }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+  getTitleText() {
+    return browser.getTitle();
+  }
+
+  Submit(){
+    return element(by.buttonText('Submit')).click();
+  }
+  setplayer(){
+    return element(by.id('max-players')).sendKeys('1');
+  }
+  login(){
+    return element(by.className('btn')).click();
+  }
+  loginengine(){
+    return element(by.className('result active access focussed')).click();
   }
 }
